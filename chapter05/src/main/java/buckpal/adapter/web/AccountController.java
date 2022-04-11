2f16a650a27592072de22125e1208131387af697
david <david@lific.io>
2022-04-12 01:10:54 +0900

    만들면서 배우는 클린 아키텍처 챕터 5 관련 예제코드

---
+++ chapter05/src/main/java/buckpal/adapter/web/AccountController.java
@@ -0,0 +1,45 @@
+package buckpal.adapter.web;
+
+import buckpal.application.port.in.SendMoneyUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+    private final GetAccountBalanceQuery getAccountBalanceQuery;
+    private final ListAccountsQuery listAccountsQuery;
+    private final LoadAccountQuery loadAccountQuery;
+
+    private final SendMoneyUseCase sendMoneyUseCase;
+    private final CreateAccountUseCase createAccountUseCase;
+
+    @GetMapping("/accounts")
+    List<AccountResource> listAccounts() {
+        ...
+    }
+
+    @GetMapping("/accounts/{accountId}")
+    AccountResource getAccount(@PathVariable("accountId") Long accountId) {
+        ...
+    }
+
+    @GetMapping("/accounts/{accountId}/balance")
+    long getAccountBalance(@PathVariable("accountId") Long accountId) {
+        ...
+    }
+
+    @PostMapping("/accounts")
+    AccountResource createAccount(@RequestBody AccountResource accountResource) {
+        ...
+    }
+
+    @PostMapping("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+    void sendMoney(
+            @PathVariable("sourceAccountId") Long sourceAccountId,
+            @PathVariable("targetAccountId") Long targetAccountId,
+            @PathVariable("amount") Long amount
+    ) {
+        ...
+    }
+}
